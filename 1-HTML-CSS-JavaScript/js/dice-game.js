aaa5381266713414245baeb085fab7c9ef239a9b
Sam Jossund <sjossund@yahoo.com>
2022-06-03 20:12:43 -0500

    first commit

---
+++ 1-HTML-CSS-JavaScript/js/dice-game.js
@@ -0,0 +1,16 @@
+function rollDice() {
+    let goldCoins = 0;
+    for (let i = 0; i < 10; i++) {
+        const roll = Math.floor(Math.random() * 6) + 1;
+        alert('You rolled a ' + roll + '.');
+        if (roll === 1) {
+            alert('Game over, no more rolls!');
+            break;
+        }
+        if (roll < 5) {
+            continue;
+        }
+        alert('Congratulations, you win ' + roll + ' gold coins!');
+    }
+    alert('You have won a total of ' + goldCoins + ' gold coins!');
+}
